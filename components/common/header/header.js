2f43d960dc2621efeaad68b49c5f82c95551d028
Alexander Hansen <Alexander-hansen@hotmail.com>
2022-06-03 10:00:44 +0200

    active links

--- components/common/header/header.js
+++ components/common/header/header.js
@@ -1,40 +1,60 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Authentication from '../../login/authentication';
+import { useRouter } from 'next/router';
 
-const Header = () => (
-  <header>
-    <div>
-      <Authentication />
-    </div>
-    <div className='logo'>
-      <Image src='/images/logo.svg' alt='Logo' width={300} height={200}></Image>
-    </div>
-    <div className='navigation'>
-      <ul>
-        <li>
-          <Link href='/'>
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/locations'>
-            <a>Lokasjoner</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/about'>
-            <a>Om oss</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/companyagreement'>
-            <a>Firmaavtale</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <header>
+      <div>
+        <Authentication />
+      </div>
+      <div className='logo'>
+        <Image
+          src='/images/logo.svg'
+          alt='Logo'
+          width={300}
+          height={200}
+        ></Image>
+      </div>
+      <div className='navigation'>
+        <ul>
+          <li>
+            <Link href='/'>
+              <a className={router.pathname == '/' ? 'active' : ''}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/locations'>
+              <a className={router.pathname == '/locations' ? 'active' : ''}>
+                Lokasjoner
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/about'>
+              <a className={router.pathname == '/about' ? 'active' : ''}>
+                Om oss
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/companyagreement'>
+              <a
+                className={
+                  router.pathname == '/companyagreement' ? 'active' : ''
+                }
+              >
+                Firmaavtale
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
