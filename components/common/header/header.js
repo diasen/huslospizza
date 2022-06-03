4946eee62f5e8c21f0b2996391021ee83f3d7dbf
Alexander Hansen <Alexander-hansen@hotmail.com>
2022-06-03 12:07:27 +0200

    hamburger menu

--- components/common/header/header.js
+++ components/common/header/header.js
@@ -2,9 +2,17 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Authentication from '../../login/authentication';
 import { useRouter } from 'next/router';
+import Hamburger from '../../hamburger/hamburger';
+import { useState } from 'react';
 
 const Header = () => {
   const router = useRouter();
+
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
 
   return (
     <header>
@@ -52,7 +60,64 @@ const Header = () => {
             </Link>
           </li>
         </ul>
+        <div className='hamburger' onClick={toggleHamburger}>
+          <Hamburger isOpen={hamburgerOpen} />
+        </div>
       </div>
+      <style jsx>{`
+        .navigation {
+          width: 100%;
+          justify-content: center;
+        }
+
+        .navigation ul {
+          display: flex;
+          flex-wrap: wrap;
+          float: right;
+          margin: 0px;
+          padding: 0px;
+          overflow: hidden;
+        }
+        .navigation ul li {
+          list-style-type: none;
+          padding-right: 10px;
+        }
+        .hamburger {
+          display: none;
+          z-index: 6;
+        }
+
+        .burger1 {
+          color: #fefefe;
+        }
+        @media (max-width: 767px) {
+          .navigation {
+            width: 100%;
+            justify-content: left;
+            background-color: #606d51;
+          }
+
+          .hamburger {
+            margin-left: 10px;
+            display: inline;
+            z-index: 0;
+            position: inline-block;
+            position: absolute;
+          }
+
+          .navigation ul {
+            display: ${hamburgerOpen ? 'inline' : 'none'};
+            background-color: #606d51;
+            width: 100%;
+            position: relative;
+            top: 115px;
+          }
+        }
+
+        li {
+          margin: 20px;
+        }
+      `}</style>
     </header>
   );
 };
